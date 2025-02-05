2b64951bdc00ad0f20dc7104589bc9f718b0298e
Huy22032002 <ndhuy2203@gmail.com>
2025-02-05 20:48:51 +0700

    'first_commit'

---
+++ src/main/java/vn/edu/iuh/fit/hsocial_subproject_nhom07/HSocialSubProjectNhom07Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.hsocial_subproject_nhom07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HSocialSubProjectNhom07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HSocialSubProjectNhom07Application.class, args);
+	}
+
+}

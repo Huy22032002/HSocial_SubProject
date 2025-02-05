10df7af028c0ed4e07a2b4e65ef485c48bf510a9
Huy22032002 <ndhuy2203@gmail.com>
2025-02-05 20:48:51 +0700

    'first_commit'

---
+++ src/test/java/vn/edu/iuh/fit/hsocial_subproject_nhom07/HSocialSubProjectNhom07ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.hsocial_subproject_nhom07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HSocialSubProjectNhom07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
